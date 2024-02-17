f62e65c9ef84bd3752bf360ccfc254cdce290422
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2024-02-17 18:05:49 +0200

    add night theme

--- src/components/WeaterApp/WeatherApp.jsx
+++ src/components/WeaterApp/WeatherApp.jsx
@@ -12,10 +12,20 @@ import { toast } from 'react-toastify';
 import { RotatingLines } from 'react-loader-spinner';
 import sun_bgc from '../Assets/sunny.gif';
 import snow_bgc from '../Assets/snow.gif';
-import rain_bgc from '../Assets/rain.gif';
 import cloud_bgc from '../Assets/cloud.gif';
 import drizzle_bgc from '../Assets/drizzle.gif';
-
+import clear_moon from '../Assets/clear-moon.png';
+import cloud_moon from '../Assets/cloud-moon.png';
+import day_drizzle from '../Assets/day-drizzle.gif';
+import day_rain from '../Assets/day-rain.gif';
+import lightning_day from '../Assets/l-day.png';
+import lightning_moon from '../Assets/l-night.png';
+import lightning_bgc from '../Assets/lig.gif';
+import mist_bgc from '../Assets/mist.gif';
+import night_clear_bgc from '../Assets/night-clear.gif';
+import night_cloud_bgc from '../Assets/night-cloud.gif';
+import night_rain_bgc from '../Assets/night-rain.gif';
+import rain_moon from '../Assets/rain-moon.png';
 
 const WeatherApp = () => {
 
@@ -30,7 +40,6 @@ const WeatherApp = () => {
     const handleChange = (e) => {
         setCity(e.target.value);
     };
-    
     
     const fetchCoordinate = async () => {
             navigator.geolocation.getCurrentPosition(async(position) => {
@@ -58,28 +67,63 @@ const WeatherApp = () => {
             }
             let result = await response.json();
             setWeather(result);
+            const weatherIcon = result.weather[0].icon;
 
-             if (result.weather[0].icon === '01d' || result.weather[0].icon === '01n') {
-                 setWicon(clear_icon);
-                 setBackground(sun_bgc);
-        } else if (result.weather[0].icon === '02d' || result.weather[0].icon === '02n') {
-                 setWicon(cloud_icon);
-                 setBackground(cloud_bgc);
-        } else if (result.weather[0].icon === '03d' || result.weather[0].icon === '03n') {
-                 setWicon(drizzle_icon);
-                 setBackground(drizzle_bgc);
-        } else if (result.weather[0].icon === '04d' || result.weather[0].icon === '04n') {
-                 setWicon(drizzle_icon);
-                 setBackground(drizzle_bgc);
-        } else if (result.weather[0].icon === '09d' || result.weather[0].icon === '09n') {
-                 setWicon(rain_icon);
-                 setBackground(rain_bgc);
-        } else if (result.weather[0].icon === '10d' || result.weather[0].icon === '10n') {
-                 setWicon(rain_icon);
-                 setBackground(rain_bgc);
-        } else if (result.weather[0].icon === '13d' || result.weather[0].icon === '13n') {
+            if (weatherIcon === '01d') {
+                setWicon(clear_icon);
+                setBackground(sun_bgc);
+            } else if (weatherIcon === '01n') {
+                setWicon(clear_moon);
+                setBackground(night_clear_bgc);
+            } else if (weatherIcon === '02d') {
+                setWicon(cloud_icon);
+                setBackground(cloud_bgc);
+            }
+            else if (weatherIcon === '02n') {
+                setWicon(cloud_moon);
+                setBackground(night_cloud_bgc);
+            } else if (weatherIcon === '03d') {
+                setWicon(cloud_icon);
+                setBackground(cloud_bgc);
+            } else if (weatherIcon === '03n') {
+                setWicon(cloud_moon);
+                setBackground(night_cloud_bgc);
+            } else if (weatherIcon === '04d') {
+                setWicon(drizzle_icon);
+                setBackground(day_drizzle);
+            } else if (weatherIcon === '04n') {
+                setWicon(drizzle_icon);
+                setBackground(drizzle_bgc);
+            } else if (weatherIcon === '09d') {
+                setWicon(rain_icon);
+                setBackground(day_rain);
+            } else if (weatherIcon === '09n') { 
+                 setWicon(rain_moon);
+                setBackground(night_rain_bgc);
+        } else if (weatherIcon === '10d') {
+                setWicon(rain_icon);
+                setBackground(day_rain);
+            } else if (weatherIcon === '10n') { 
+                 setWicon(rain_moon);
+                setBackground(night_rain_bgc);
+        } else if (weatherIcon === '11d') {
+                setWicon(lightning_day);
+                setBackground(lightning_bgc);
+            } else if (weatherIcon === '11n') { 
+                 setWicon(lightning_moon);
+                setBackground(lightning_bgc);
+        } else if (weatherIcon === '13d') {
+                setWicon(snow_icon);
+                setBackground(snow_bgc);
+            } else if (weatherIcon === '13n') { 
                  setWicon(snow_icon);
-                 setBackground(snow_bgc);
+                setBackground(snow_bgc);
+        } else if (weatherIcon === '50d') {
+                setWicon(clear_icon);
+                setBackground(mist_bgc);
+            } else if (weatherIcon === '50n') { 
+                 setWicon(clear_moon);
+                setBackground(mist_bgc);
         } else {
                  setWicon(clear_icon);
                  setBackground(sun_bgc);
@@ -141,10 +185,11 @@ useEffect(() => {
           </form>
           <div className="weather__img">
               <img src={wicon} alt=" cloud icon" />
-          </div>
+                    </div>
+                    <div className="weather__location">{weather.name}</div>
           <div className="weather__temp">{Math.floor(weather.main.temp)} °c
           </div>
-                    <div className="weather__location">{weather.name}</div>
+                    <h3 className='weather__title'>{weather.weather[0].main}</h3>
           <div className="data__container">
               <div className="element">
                   <img src={humidity_icon} alt="humidity icon" className="icon" />
